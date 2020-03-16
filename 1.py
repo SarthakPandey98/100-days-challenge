a3057608719b09de2697d018e5f58bb22e622c94
SarthakPandey98 <53701252+SarthakPandey98@users.noreply.github.com>
2020-03-17 02:24:18 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+# Day1
+# Euler problem 1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+# Starting off easy
+multiples = [] 
+total = 0
+for i in range(1, 1000):
+    if i % 3 == 0 or i % 5 == 0:
+        total= total + i
+        multiples.append(i)
+for i in range (0,len(multiples)):
+    print(multiples[i])
+print(total) 
